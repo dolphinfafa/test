134fcf98ebe4fbdfa148db6a90c40135e1582d1c
dolphinfafa <leafleaving@gmail.com>
2024-11-18 23:43:05 +0800

    coin update

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import random
 
-# 随机抛n次硬币，统计最多连续出现的正面次数
-def coin(n):
+def coin():
+    n_tosses = 10000
+    coin_tosses = [random.randint(0, 1) for _ in range(n_tosses)]  # 1表示正面，0表示反面
+
+    # 统计最多连续出现的正面次数
     max_count = 0
     count = 0
-    for i in range(n):
-        if random.randint(0, 1) == 1:
+    for i in range(n_tosses):
+        if coin_tosses[i] == 1:
             count += 1
             if count > max_count:
                 max_count = count
@@ -14,24 +17,20 @@ def coin(n):
     return max_count
 
 # 测试1000次，每次抛10000次硬币
-# 统计每次的值，并且统计这个值出现的次数
-# 输出最多连续出现的正面次数及其出现次数
+# 统计每次的值，并且统计这个值出现的次数,记录在字典d中
+# 字典d按key的value从大到小排序，并打印
 def test():
     d = {}
     for i in range(1000):
-        n = coin(10000)
+        n = coin()
         if n in d:
             d[n] += 1
         else:
             d[n] = 1
-    max_count = max(d.keys())
     
     # 按d[key]的值从大到小排序
     d = sorted(d.items(), key=lambda x: x[1], reverse=True)
     print(d)
-    print("最多连续出现的正面次数及其出现次数：")
-    print("max_count: ", max_count)
-    print("count: ", d[0][1])
 
 if __name__ == '__main__':
     test()
